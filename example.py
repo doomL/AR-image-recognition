2628378be433aecef529ad23f99b42ec40cd389a
Matteo Grande <34687892+BigMet@users.noreply.github.com>
2019-05-07 19:57:49 +0200

    Add files via upload
    Accende la web cam ma non riprende

--- example.py
+++ example.py
@@ -4,13 +4,21 @@ import numpy as np
 import argparse
 parser = argparse.ArgumentParser(description='Code for Feature Matching with FLANN tutorial.')
 parser.add_argument('--input1', help='Path to input image 1.', default='10.jpg')
-parser.add_argument('--input2', help='Path to input image 2.', default='foto.jpg')
+parser.add_argument('--input2', help='Path to input image 2.', default='2.jpg')
 args = parser.parse_args()
 img1 = cv.imread(args.input1)
 img2 = cv.imread(args.input2)
 if img1 is None or img2 is None:
     print('Could not open or find the images!')
     exit(0)
+
+
+
+
+
+
+videoCapture = cv.VideoCapture(0)
+
 #-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
 minHessian = 200
 detector = cv.xfeatures2d_SURF.create(hessianThreshold=minHessian)
@@ -26,15 +34,22 @@ good_matches = []
 for m,n in knn_matches:
     if m.distance < ratio_thresh * n.distance:
         good_matches.append(m)
-print("Ho trovato questi punti giusti",len(good_matches))
+print("Punti trovati",len(good_matches))
 
 if len(good_matches)>=10:
     print("Oggetto trovato")
 #-- Draw matches
 img_matches = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], 3), dtype=np.uint8)
 cv.drawMatches(img1, keypoints1, img2, keypoints2, good_matches, img_matches, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+
 #-- Show detected matches
-#cv.resizeWindow('img_matches', 2,2)
-cv.imshow('Good Matches', img_matches)
+# cv.imshow('Good Matches', img_matches)
 cv.imwrite('sift.jpg',img_matches)
+
+while True:
+    ret, frame = videoCapture.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('Camera Open Maybe',gray)
+videoCapture.release()
 cv.waitKey()
